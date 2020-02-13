7ba4715c5b3f1ba24b784c5d0b96df8c6494d03d
Muhammad Jazzel Mehmood <jazzelmehmood4@gmail.com>
2020-02-13 19:17:54 +0500

    Initial commit

--- src/accounts/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import (ProfileUpdateApiView)
-app_name = 'accounts-api'
-urlpatterns = [
-    path('update/<int:pk>', ProfileUpdateApiView.as_view(), name='create'),
-
-]
--- src/portfolio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import (FileFieldView)
-app_name = 'portfolio'
-
-urlpatterns = [
-    # path('', ServiceListView.as_view(), name='list'),
-    path('add/', FileFieldView.as_view(), name='add'),
-
-]
