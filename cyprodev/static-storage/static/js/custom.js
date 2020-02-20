98ba8f3cc47c6864b1adc7d82d38abb275ce2ea5
Muhammad Jazzel Mehmood <jazzelmehmood4@gmail.com>
2020-02-20 22:44:01 +0500

    All bugs fixed

--- cyprodev/static-storage/static/js/custom.js
+++ cyprodev/static-storage/static/js/custom.js
@@ -1,7 +1,5 @@
 jQuery(function ($) {
-    'use strict';
-    var body = $('#body');
-    $(".loader").delay(1000).fadeOut("slow"); $("#overlayer").delay(1000).fadeOut("slow"); var siteMenuClone = function () {
+    'use strict'; $(".loader").delay(1000).fadeOut("slow"); $("#overlayer").delay(1000).fadeOut("slow"); var siteMenuClone = function () {
         $('.js-clone-nav').each(function () { var $this = $(this); $this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body'); }); setTimeout(function () { var counter = 0; $('.site-mobile-menu .has-children').each(function () { var $this = $(this); $this.prepend('<span class="arrow-collapse collapsed">'); $this.find('.arrow-collapse').attr({ 'data-toggle': 'collapse', 'data-target': '#collapseItem' + counter, }); $this.find('> ul').attr({ 'class': 'collapse', 'id': 'collapseItem' + counter, }); counter++; }); }, 1000); $('body').on('click', '.arrow-collapse', function (e) {
             var $this = $(this); if ($this.closest('li').find('.collapse').hasClass('show')) { $this.removeClass('active'); } else { $this.addClass('active'); }
             e.preventDefault();
@@ -11,11 +9,13 @@ jQuery(function ($) {
     }; siteMenuClone(); var sitePlusMinus = function () { $('.js-btn-minus').on('click', function (e) { e.preventDefault(); if ($(this).closest('.input-group').find('.form-control').val() != 0) { $(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) - 1); } else { $(this).closest('.input-group').find('.form-control').val(parseInt(0)); } }); $('.js-btn-plus').on('click', function (e) { e.preventDefault(); $(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) + 1); }); }; var siteIstotope = function () { var $container = $('#posts').isotope({ itemSelector: '.item', isFitWidth: true }); $(window).resize(function () { $container.isotope({ columnWidth: '.col-sm-3' }); }); $container.isotope({ filter: '*' }); $('#filters').on('click', 'button', function (e) { e.preventDefault(); var filterValue = $(this).attr('data-filter'); $container.isotope({ filter: filterValue }); $('#filters button').removeClass('active'); $(this).addClass('active'); }); }
     siteIstotope(); var fancyBoxInit = function () { $('.fancybox').on('click', function () { var visibleLinks = $('.fancybox'); $.fancybox.open(visibleLinks, {}, visibleLinks.index(this)); return false; }); }
     fancyBoxInit(); var stickyFillInit = function () { $(window).on('resize orientationchange', function () { recalc(); }).resize(); function recalc() { if ($('.jm-sticky-top').length > 0) { var elements = $('.jm-sticky-top'); Stickyfill.add(elements); } } }
-    stickyFillInit(); var OnePageNavigation = function () {
-        // var navToggler = $('.site-menu-toggle'); $("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function (e) {
-        //     e.preventDefault(); var hash = this.hash;
-        //     $('html, body').animate({ 'scrollTop': $(hash).offset().top }, 600, 'easeInOutCirc', function () { window.location.hash = hash; });
-        // });
+    stickyFillInit();
+    var OnePageNavigation = function () {
+        var navToggler = $('.site-menu-toggle');
+        $("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function (e) {
+            e.preventDefault();
+            var hash = this.hash; $('html, body').animate({ 'scrollTop': $(hash).offset().top }, 600, 'easeInOutCirc', function () { window.location.hash = hash; });
+        });
     }; OnePageNavigation(); var counterInit = function () {
         if ($('.section-counter').length > 0) {
             $('.section-counter').waypoint(function (direction) {
