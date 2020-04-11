e9459644cb7c7a75618027d6150ebf48d94fab60
Muhammad Jazzel Mehmood <jazzelmehmood4@gmail.com>
2020-04-11 20:48:09 +0500

    Login page is now according to theme

--- cyprodev/contact/views.py
+++ cyprodev/contact/views.py
@@ -3,6 +3,8 @@ from django.views.generic import CreateView, ListView, DetailView
 from .forms import ContactForm, ProjectForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Contact, Project
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 
@@ -10,6 +12,12 @@ class ContactCreateView(CreateView):
     form_class = ContactForm
     template_name = 'contact/contact_form.html'
     success_url = '/contact-us/'
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(
+            self.request, "Message delivered to admins. Someone from #CyproDev will contact you soon !!")
+        return HttpResponseRedirect('/contact-us/')
 
     def get_context_data(self, **kwargs):
         context = super(ContactCreateView, self).get_context_data(**kwargs)
@@ -65,12 +73,17 @@ class ProjectCreateView(CreateView):
     template_name = 'contact/project_form.html'
     success_url = '/start-your-project/'
 
+    def form_valid(self, form):
+        form.save()
+        messages.success(
+            self.request, "Message delivered to admins. Someone from #CyproDev will contact you soon !!")
+        return HttpResponseRedirect('/contact-us/')
+
     def get_context_data(self, **kwargs):
         context = super(ProjectCreateView, self).get_context_data(**kwargs)
         context['head'] = 'Start Your Project'
         context['slider_head'] = 'Start Your Project'
         context['slider_sub_head'] = ''
         context['slider_image'] = '/static/static/img/startyourproject.jpg'
-
 
         return context
